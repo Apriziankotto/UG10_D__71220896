69a0fdca7b6fa7cd560c908a7feff885c9c09cd7
Apriziankotto <117901091+Apriziankotto@users.noreply.github.com>
2022-11-24 16:05:48 +0700

    Add files via upload

---
+++ 1_D_71220896.py
@@ -0,0 +1,12 @@
+# Selamat datang di kalkulator sederhana
+print("ketik 1 untuk menghitung penjumlahan")
+print("ketik 2 untuk menghitung pengurangan")
+print("ketik 3 untuk menghitung perkalian")
+print("ketik 4 untuk menghitung pembagian")
+print("ketik 5 untuk menghitung sisa hasil bagi(modulus)")
+print("ketik 6 untuk menghitung pemangkatan")
+n= input("Masukkan pilihan anda: ")
+m= input("Masukkan bilangan pertama: ")
+l= input("Masukkan bilangan kedua: ")
+Hasil=m*l
+Print("Hasil dari{} di{} dengan {} adalah {}", format,(n,m,l,hasil)
